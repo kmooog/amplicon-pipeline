efd11e1445913080b6c7c0519fa2cbc29d66d638
Yohei Kumagai <ryujhbv@gmail.com>
2020-02-25 11:49:05 +0900

    update

--- script/2-parse_preprocessed_data/parse_preprocessed_data.sh
+++ script/2-parse_preprocessed_data/parse_preprocessed_data.sh
@@ -2,4 +2,4 @@
 #$ -cwd
 #$ -N amplicon-pipeline-2
 module load singularity
-singularity exec /home/kumay/test_container/amplicon-pipeline-latest.img cwltool /root/amplicon-pipeline/script/2-parse_preprocessed_data/amplicon_analysis_workflow.cwl --merged_fasta all_amplicon_seqs.fa
+singularity exec /home/kumay/test_container/amplicon-pipeline-latest.img cwltool /root/amplicon-pipeline/script/2-parse_preprocessed_data/amplicon_analysis_workflow.cwl --merged_fasta all_amplicon_seqs.fa --taxmap /home/kumay/database/taxmap_slv_ssu_ref_132.txt
