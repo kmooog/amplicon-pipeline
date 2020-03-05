0b9af35a2d5eeb6627cf9f9bd8326de1141bee30
kmooog <ryujhbv@gmail.com>
2020-03-05 16:59:32 +0900

    update

--- script/2-parse_preprocessed_data/remove_hyphen.py
+++ script/2-parse_preprocessed_data/remove_hyphen.py
@@ -3,8 +3,11 @@ import sys
 sys.path.append("/usr/local/lib/python3.6/dist-packages")
 from Bio import SeqIO
 
+argvs = sys.argv
+centroids_remove_tripleton = argvs[1]
+
 with open("centroids_remove_tripleton_hyphen_removed.fa","w") as out:
-   for record in SeqIO.parse(open("centroids_remove_tripleton.fa"), "fasta"):
+   for record in SeqIO.parse(open(centroids_remove_tripleton), "fasta"):
       tmpstr = str(record.id)
       tmpseq = str(record.seq).replace("-","N")
       out.write(">" + tmpstr + "\n" + tmpseq + "\n")
